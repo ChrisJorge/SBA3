3da0d6dbc19ffda09954a27b1d42dc68dda6d395
Chris <Christopherjorge47@icloud.com>
2024-03-29 14:58:06 -0400

    Added JS Logic to choose word from the word list

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let word_list = ['Example', 'test', 'dictionary', 'Per Scholas', 'Coding', 'Software'];
 
-let random_choice = Math.floor(Math.random() * 6)
-console.log(random_choice)
+let random_choice = Math.floor(Math.random() * 6);
+
+let word = word_list[random_choice];
+
+
