677a8da0e864825047e7183ba446801ed7b5b3f9
Chris <Christopherjorge47@icloud.com>
2024-03-30 10:52:05 -0400

    Created JS Logic to have number of boxes corresponding to number of letters in word appear in wordContainer

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ let random_choice = Math.floor(Math.random() * 6);
 
 let word = word_list[random_choice];
 
+let container = document.getElementById('wordContainer');
 
+for(let i = 0; i < word.length; i++)
+{
+    let letter = document.createElement('div');
+    letter.setAttribute('class', 'letter');
+    container.appendChild(letter);
+}
