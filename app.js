ebd0be4f891ae58b8af461cf24416fe9140da4ac
Chris <Christopherjorge47@icloud.com>
2024-03-30 11:57:44 -0400

    Added Js Logic to add letters to boxes if correct letter is guessed

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-let word_list = ['Example', 'test', 'dictionary', 'Per Scholas', 'Coding', 'Software'];
+let word_list = ['example', 'test', 'dictionary', 'perscholas', 'coding', 'software'];
 
 let random_choice = Math.floor(Math.random() * 6);
 
 let word = word_list[random_choice];
 let word_arr = []
+let guessed_letters = []
 
 for(let i = 0; i < word.length; i++)
 {
@@ -14,13 +15,38 @@ let container = document.getElementById('wordContainer');
 
 for(let i = 0; i < word.length; i++)
 {
-    let letter = document.createElement('div');
-    letter.setAttribute('class', 'letter');
-    container.appendChild(letter);
+    let letterBox = document.createElement('div');
+    letterBox.setAttribute('class', 'letter');
+    container.appendChild(letterBox);
 }
 
 let submitBtn = document.querySelector(".submit-btn")
 submitBtn.addEventListener('click', () => {
-    let char = document.getElementById('input')
-    console.log(char.value)
+    let char = document.getElementById('input');
+    let letter = char.value;
+    letter = letter.toLowerCase();
+    guess(letter);
+    
 })
+console.log(word)
+const guess = (letter) => {
+    this.letter = letter;
+    
+    if(guessed_letters.includes(letter))
+    {
+        console.log('Letter has already been guessed');
+        return;
+    }
+    guessed_letters.push(letter)
+
+    if(word_arr.includes(letter))
+    {
+        let index = (word_arr.indexOf(letter))
+        let box = document.querySelectorAll('.letter')[index]
+        box.innerHTML = letter
+    } 
+    else {
+        console.log('letter not in word')
+    }
+
+}
