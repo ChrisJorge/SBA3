744b4207dac7e590a8b7a38369cd0171c6942d36
Chris <Christopherjorge47@icloud.com>
2024-03-30 14:05:26 -0400

    Added clarifying comments to code

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
+// Create a list of words
 let word_list = ['computer', 'test', 'object', 'perscholas', 'coding', 'software', 'integer', 'boolean', 'float', 'array'];
 
+// Get a random number from 0 - 9 (indexes of the word_list array)
 let random_choice = Math.floor(Math.random() * 10);
 
+// Assign the word randomly and instantiate other necessary variables
 let word = word_list[random_choice];
 let word_arr = []
 let guessed_letters = []
 let approved  = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let numGuesses = 5;
 
+// Cache document variables necessary for functionality
 let guessesLeft = document.querySelector('.attemptsLeft')
+let container = document.getElementById('wordContainer');
 
+// Push each letter of the chosen word into an array
 for(let i = 0; i < word.length; i++)
 {
     word_arr.push(word[i])
 }
 
-let container = document.getElementById('wordContainer');
 
+// Get number of spots (Used to check if user won)
 let guessSpots = word_arr.length
+
+// Create the necessary number of boxes based on number of letters in word
 for(let i = 0; i < word.length; i++)
 {
     let letterBox = document.createElement('div');
@@ -25,35 +33,47 @@ for(let i = 0; i < word.length; i++)
     container.appendChild(letterBox);
 }
 
+// Event listener for submit button
 let submitBtn = document.querySelector(".submit-btn")
 submitBtn.addEventListener('click', () => {
+    // Function converting users letter to lowercase and resetting input field
     let letter = getLetter()
+    // Function which handles if the input is valid, correct, or incorrect
     guess(letter);
     
 })
 
+// Event listener for the enter key
 window.addEventListener('keydown', (event) => {
    if(event.key === 'Enter')
    {
+    // Function converting users letter to lowercase and resetting input field
     let letter = getLetter()
+     // Function which handles if the input is valid, correct, or incorrect
     guess(letter)
    }
+
 })
+
 // For Testing Purposes
 console.log(word)
 // _______________________________
 
 const guess = (letter) => {
     this.letter = letter;
+    // Cache variable used to display to user if guess was correct, incorrect, or invalid
     let announcement = document.querySelector('.upperContainer').lastElementChild;
+    // Check users input against approved inputs
     if(!approved.includes(letter))
     {
         announcement.style.color = "red";
         announcement.innerHTML = "Guess is not a letter, try again"
         return;
     }
+    // Cache location of the guessed letters paragraph element
     let guessed = document.querySelector('.guessedLetters')
     
+    // Check if guessed letter has been guessed already
     if(guessed_letters.includes(letter))
     {
       announcement.style.color = "red"
@@ -62,8 +82,10 @@ const guess = (letter) => {
     }
     guessed_letters.push(letter)
 
+    // Check if the guessed letter is part of the word
     if(word_arr.includes(letter))
     {
+        // Function that adds the letter into its corresponding box
         searchForLetter(letter)
         announcement.style.color = "green"
         announcement.innerHTML = `Guessed Letter ${letter.toUpperCase()} is in the word!`
@@ -81,23 +103,26 @@ const guess = (letter) => {
 
 }
 
+// Checks if the game is still in progress, or if it is over
 const gameActive = (count, spots) => {
     this.count = count;
+    this.spots = spots
     let announcement = document.querySelector('.upperContainer').lastElementChild;
-    if(guessSpots === 0)
+    if(spots === 0)
     {
         announcement.style.color = "green";
         announcement.innerHTML = "You correctly guessed the word!!!";
         disabled()
         return;
     }
-    
+
     if (count > 0){
         
     }
     else {
         announcement.style.color = "red";
         announcement.innerHTML = "Game Over: Unable to guess the word";
+        // Function that disabled the input field and submit button
         disabled();
         let container = document.querySelector('.inputContainer');
         let btn = document.createElement('button');
@@ -144,6 +169,7 @@ const getLetter = () => {
     return letter;
 }
 
+// Function that shows the word if the user looses and clicks on the button
 const show = () => 
 {
    for(let i = 0; i < word_arr.length; i++)
