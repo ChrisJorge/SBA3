1e605c220039e85b67fff20e187125bbe45770ad
Chris <Christopherjorge47@icloud.com>
2024-03-30 10:56:58 -0400

    Created JS Logic to have letters from word put into an array

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ let word_list = ['Example', 'test', 'dictionary', 'Per Scholas', 'Coding', 'Soft
 let random_choice = Math.floor(Math.random() * 6);
 
 let word = word_list[random_choice];
+let word_arr = []
+
+for(let i = 0; i < word.length; i++)
+{
+    word_arr.push(word[i])
+}
 
 let container = document.getElementById('wordContainer');
 
